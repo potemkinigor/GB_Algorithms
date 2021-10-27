8d562ee49779361929001b7491ebc845c0b07db4
Igor Potemkin <igor.potemkin@hiq.ru>
2021-10-27 13:54:08 +0300

    Lesson 3
    1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения оптимизированной и не оптимизированной программы. Написать функции сортировки, которые возвращают количество операций.
    
    2. *Реализовать шейкерную сортировку.
    
    3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив. Функция возвращает индекс найденного элемента или -1, если элемент не найден.
    
    4. *Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма.

--- Algorythms_lesson1/Algorythms_lesson1/main.c
+++ Algorythms_lesson1/Algorythms_lesson1/main.c
@@ -5,40 +5,39 @@
 //  Created by Igor Potemkin on 15.10.2021.
 //
 
+//1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения оптимизированной и не оптимизированной программы. Написать функции сортировки, которые возвращают количество операций.
+//
+//2. *Реализовать шейкерную сортировку.
+//
+//3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив. Функция возвращает индекс найденного элемента или -1, если элемент не найден.
+//
+//4. *Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма.
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
-//MARK - Main functions protocol
+void launchSorting(void);
+void binarySearch(void);
 
-void transformFromDecimalToBinary(void);
-void raiseToDegreeLinear(void);
-void raiseToDegreeRecursion(void);
-void raiseToDegreeRecursionWithEvenProperty(void);
-
-//MARK - Support functions protocol
-
-int transformToBinary(int number);
-double transformToDegreeWithRecursion(double number, int degree);
+void simpleBubbleSorting(int *initialArray, int arraySize);
+void bubbleSortingOptimized(int *initialArray, int arraySize);
+int * shakerSorting(int *initialArray, int arraySize);
 void menu(void);
 
 int main(int argc, const char * argv[]) {
     int sel = 0;
-    
+
     do {
         menu();
         scanf("%i", &sel);
         switch (sel) {
             case 1:
-                transformFromDecimalToBinary();
+                launchSorting();
                 break;
             case 2:
-                raiseToDegreeLinear();
-                break;
-            case 3:
-                raiseToDegreeRecursion();
-                break;
-            case 4:
-                raiseToDegreeRecursionWithEvenProperty();
+                binarySearch();
                 break;
             case 0:
                 printf("Bye-bye! See you next time\n");
@@ -48,107 +47,352 @@ int main(int argc, const char * argv[]) {
                 break;
         }
     } while (sel != 0);
-    
+
     return 0;
 }
 
-// MARK: - 1
-
-void transformFromDecimalToBinary() {
+void launchSorting(void) {
+    int arraySize;
+    int index;
     
-    int number;
+    printf("Enter array size, please: ");
+    scanf("%i", &arraySize);
     
-    printf("Enter required number: ");
-    scanf("%i", &number);
+    int initialArray[arraySize];
+    int initialArrayForOptimizedSorting[arraySize];
+    int initialArrayForShakerSorting[arraySize];
     
-    printf("Reversed result is: \n");
-    transformToBinary(number);
+    for (index = 0; index < arraySize; index++) {
+        int random = rand()%arraySize;
+        initialArray[index] = random;
+        initialArrayForOptimizedSorting[index] = random;
+        initialArrayForShakerSorting[index] = random;
+    }
+    
+    printf("Initial values for array are:\n");
+    for (index = 0; index < arraySize; index++) {
+        printf("%i ", initialArray[index]);
+    }
     printf("\n");
+    
+    simpleBubbleSorting(initialArray, arraySize);
+    bubbleSortingOptimized(initialArrayForOptimizedSorting, arraySize);
+    shakerSorting(initialArrayForShakerSorting, arraySize);
 }
 
-int transformToBinary(int number) {
-    if (number % 2 == 0) {
-        printf("0");
-    } else {
-        printf("1");
+void simpleBubbleSorting(int *initialArray, int arraySize) {
+    int i, j;
+    int comparisonCounter;
+    int swapCounter;
+    
+    comparisonCounter = 0;
+    swapCounter = 0;
+    
+    for (i = 0; i < arraySize; i++) {
+        for (j = 0; j < arraySize - 1; j++) {
+            comparisonCounter++;
+            if (initialArray[j] > initialArray[j + 1]) {
+                int temp;
+                swapCounter++;
+                temp = initialArray[j];
+                initialArray[j] = initialArray[j + 1];
+                initialArray[j + 1] = temp;
+            }
+        }
     }
     
-    if (number < 2) {
-        return 0;
-    } else {
-        return transformToBinary(number / 2);
+    printf("\n");
+    printf("With simple bubble sorting we will have:\n");
+    for (i = 0; i < arraySize; i++) {
+        printf("%i ", initialArray[i]);
     }
+    printf("\n");
+    printf("Comparison iterations: %i\n", comparisonCounter);
+    printf("Swap iterations: %i\n", swapCounter);
+    printf("O(n^2) should be %lf, but is %i\n", pow(arraySize, 2), comparisonCounter + swapCounter);
 }
 
-// MARK: - 2
-
-void raiseToDegreeLinear(void) {
-    double a;
-    int i, n;
-    double result;
+void bubbleSortingOptimized(int *initialArray, int arraySize) {
+    int i, j;
+    int comparisonCounter;
+    int swapCounter;
     
-    printf("Enter required number and degree: ");
-    scanf("%lf %i", &a, &n);
+    comparisonCounter = 0;
+    swapCounter = 0;
     
-    result = a;
-    
-    for (i = 1; i < n; i++) {
-        result = result * a;
-    }
-    
-    printf("Linear result is %lf\n", result);
-}
-
-// MARK: - 3
-
-void raiseToDegreeRecursion(void) {
-    double a, result;
-    int n;
-    
-    printf("Enter required number and degree: ");
-    scanf("%lf %i", &a, &n);
-    
-    result = transformToDegreeWithRecursion(a, n);
-    
-    printf("Linear result is %lf\n", result);
-}
-
-// MARK: - 4
-
-void raiseToDegreeRecursionWithEvenProperty(void) {
-    double a, result;
-    int n;
-    
-    printf("Enter required number and degree: ");
-    scanf("%lf %i", &a, &n);
-    
-    if (n % 2 == 0 ) {
-        result = transformToDegreeWithRecursion(a, n / 2);
-        result = pow(result, 2);
-    } else {
-        result = transformToDegreeWithRecursion(a, n);
+    for (i = 0; i < arraySize; i++) {
+        for (j = 0; j < arraySize - 1 - i; j++) {
+            comparisonCounter++;
+            if (initialArray[j] > initialArray[j + 1]) {
+                int temp;
+                swapCounter++;
+                temp = initialArray[j];
+                initialArray[j] = initialArray[j + 1];
+                initialArray[j + 1] = temp;
+            }
+        }
     }
     
-    printf("Linear result is %lf\n", result);
+    printf("\n");
+    printf("With optimized bubble sorting we will have:\n");
+    for (i = 0; i < arraySize; i++) {
+        printf("%i ", initialArray[i]);
+    }
+    printf("\n");
+    printf("Comparison iterations: %i\n", comparisonCounter);
+    printf("Swap iterations: %i\n", swapCounter);
+    printf("O(n^2) should be %lf, but is %i\n", pow(arraySize, 2), comparisonCounter + swapCounter);
 }
 
-// MARK: - Support
+int * shakerSorting(int *initialArray, int arraySize) {
+    int i, j;
+    int comparisonCounter;
+    int swapCounter;
+    
+    comparisonCounter = 0;
+    swapCounter = 0;
+    
+    for (i = 0; i < arraySize; i++) {
+        if (i % 2 == 0) {
+            for (j = 0; j < arraySize - 1 - i; j++) {
+                comparisonCounter++;
+                if (initialArray[j] > initialArray[j + 1]) {
+                    int temp;
+                    swapCounter++;
+                    temp = initialArray[j];
+                    initialArray[j] = initialArray[j + 1];
+                    initialArray[j + 1] = temp;
+                }
+            }
+        } else {
+            for (j = arraySize - 1; j > 0; j--) {
+                comparisonCounter++;
+                if (initialArray[j - 1] > initialArray[j]) {
+                    int temp;
+                    swapCounter++;
+                    temp = initialArray[j - 1];
+                    initialArray[j - 1] = initialArray[j];
+                    initialArray[j] = temp;
+                }
+            }
+        }
+    }
+    
+    printf("\n");
+    printf("With shaker sorting we will have:\n");
+    for (i = 0; i < arraySize; i++) {
+        printf("%i ", initialArray[i]);
+    }
+    printf("\n");
+    printf("Comparison iterations: %i\n", comparisonCounter);
+    printf("Swap iterations: %i\n", swapCounter);
+    printf("O(n^2) should be %lf, but is %i\n", pow(arraySize, 2), comparisonCounter + swapCounter);
+    
+    return initialArray;
+}
 
-double transformToDegreeWithRecursion(double number, int degree) {
-    if (degree == 1) {
-        return number;
-    } else {
-        return number * transformToDegreeWithRecursion(number, degree -= 1);
+void binarySearch(void) {
+    int arraySize;
+    int index;
+    int searchNumber;
+    
+    printf("Enter array size, please: ");
+    scanf("%i", &arraySize);
+    
+    int array[arraySize];
+    
+    for (index = 0; index < arraySize; index++) {
+        int random = rand()%arraySize;
+        array[index] = random;
+    }
+    
+    printf("Initial values for array are:\n");
+    for (index = 0; index < arraySize; index++) {
+        printf("%i ", array[index]);
+    }
+    printf("\n\n");
+    
+    int *sortedArray = shakerSorting(array, arraySize);
+    
+    printf("Enter number we are looking for: ");
+    scanf("%i", &searchNumber);
+    
+    int min = 0;
+    int max = arraySize - 1;
+    
+    if (searchNumber > sortedArray[arraySize - 1] || searchNumber < sortedArray[0]) {
+        printf("Oops, there is no such a number!\n\n");
+        return;
+    }
+    
+    while (min <= max) {
+        int mid;
+        
+        if (min == max && sortedArray[min] != searchNumber) {
+            if (sortedArray[min] == searchNumber) {
+                printf("Index of element we are looking for is %i\n\n", min + 1);
+                break;
+            } else {
+                printf("Oops, there is no such a number!\n\n");
+                break;
+            }
+        } else {
+            mid = min + (max-min) * (searchNumber - sortedArray[min])/ (sortedArray[max] - sortedArray[min]);
+        }
+        
+        if (sortedArray[mid] == searchNumber) {
+            printf("Index of element we are looking for is %i\n", mid + 1);
+            break;
+        } else if (sortedArray[mid] < searchNumber) {
+            min = mid + 1;
+        } else if (sortedArray[mid] > searchNumber) {
+            max = mid - 1;
+        }
     }
 }
 
 void menu() {
-    printf("1 - Transform from decimal to binary\n");
-    printf("2 - Raise to degree without recursion\n");
-    printf("3 - Raise to degree with recursion\n");
-    printf("4 - Raise to degree with recursion and with even property\n");
+    printf("1 - Bubble sorting algorithms\n");
+    printf("2 - Binary search\n");
     printf("0 - Exit\n");
 }
+
+
+// MARK: - Lesson 2
+
+//void transformFromDecimalToBinary(void);
+//void raiseToDegreeLinear(void);
+//void raiseToDegreeRecursion(void);
+//void raiseToDegreeRecursionWithEvenProperty(void);
+//
+//
+//int transformToBinary(int number);
+//double transformToDegreeWithRecursion(double number, int degree);
+//void menu(void);
+//
+//int main(int argc, const char * argv[]) {
+//    int sel = 0;
+//
+//    do {
+//        menu();
+//        scanf("%i", &sel);
+//        switch (sel) {
+//            case 1:
+//                transformFromDecimalToBinary();
+//                break;
+//            case 2:
+//                raiseToDegreeLinear();
+//                break;
+//            case 3:
+//                raiseToDegreeRecursion();
+//                break;
+//            case 4:
+//                raiseToDegreeRecursionWithEvenProperty();
+//                break;
+//            case 0:
+//                printf("Bye-bye! See you next time\n");
+//                break;
+//            default:
+//                printf("Something went wrong, please try again\n");
+//                break;
+//        }
+//    } while (sel != 0);
+//
+//    return 0;
+//}
+//
+//
+//void transformFromDecimalToBinary() {
+//
+//    int number;
+//
+//    printf("Enter required number: ");
+//    scanf("%i", &number);
+//
+//    printf("Reversed result is: \n");
+//    transformToBinary(number);
+//    printf("\n");
+//}
+//
+//int transformToBinary(int number) {
+//    if (number % 2 == 0) {
+//        printf("0");
+//    } else {
+//        printf("1");
+//    }
+//
+//    if (number < 2) {
+//        return 0;
+//    } else {
+//        return transformToBinary(number / 2);
+//    }
+//}
+//
+//
+//void raiseToDegreeLinear(void) {
+//    double a;
+//    int i, n;
+//    double result;
+//
+//    printf("Enter required number and degree: ");
+//    scanf("%lf %i", &a, &n);
+//
+//    result = a;
+//
+//    for (i = 1; i < n; i++) {
+//        result = result * a;
+//    }
+//
+//    printf("Linear result is %lf\n", result);
+//}
+//
+//
+//void raiseToDegreeRecursion(void) {
+//    double a, result;
+//    int n;
+//
+//    printf("Enter required number and degree: ");
+//    scanf("%lf %i", &a, &n);
+//
+//    result = transformToDegreeWithRecursion(a, n);
+//
+//    printf("Linear result is %lf\n", result);
+//}
+//
+//
+//void raiseToDegreeRecursionWithEvenProperty(void) {
+//    double a, result;
+//    int n;
+//
+//    printf("Enter required number and degree: ");
+//    scanf("%lf %i", &a, &n);
+//
+//    if (n % 2 == 0 ) {
+//        result = transformToDegreeWithRecursion(a, n / 2);
+//        result = pow(result, 2);
+//    } else {
+//        result = transformToDegreeWithRecursion(a, n);
+//    }
+//
+//    printf("Linear result is %lf\n", result);
+//}
+//
+//
+//double transformToDegreeWithRecursion(double number, int degree) {
+//    if (degree == 1) {
+//        return number;
+//    } else {
+//        return number * transformToDegreeWithRecursion(number, degree -= 1);
+//    }
+//}
+//
+//void menu() {
+//    printf("1 - Transform from decimal to binary\n");
+//    printf("2 - Raise to degree without recursion\n");
+//    printf("3 - Raise to degree with recursion\n");
+//    printf("4 - Raise to degree with recursion and with even property\n");
+//    printf("0 - Exit\n");
+//}
 
 // MARK: - Lesson 1
 
